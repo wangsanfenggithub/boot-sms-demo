3594fec4513a924bb2a2b68da9f76af3d1748de5
wangGit <690163977@qq.com>
2020-02-05 15:39:36 +0800

    版本测试2222

---
+++ src/main/java/com/boot/bootsmsdemo/BootSmsDemoApplication.java
@@ -0,0 +1,15 @@
+package com.boot.bootsmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class BootSmsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSmsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boot/bootsmsdemo/BootSmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.bootsmsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSmsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
